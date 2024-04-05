2a404e0f7d769f6c1f956674a7ea220caa1b90e8
unknown <anumahjoshuaeneye@gmail.com>
2024-04-05 15:35:30 +0100

    logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@/components/ui/button";
-import { Loader2 } from "lucide-react";
+import { ModeToggle } from "@/components/ModeToggle";
+import NavBar from "@/components/navbar/Navbar";
 
 export default function Home() {
   return (
-    <Button>
-      <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-      Please wait
-    </Button>
+    <main>
+      <NavBar />
+      <ModeToggle />
+    </main>
   );
 }
---
+++ components/navbar/NavBar.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Logo from "./Logo";
+
+const NavBar = () => {
+  return (
+    <nav className="">
+      <Logo />
+    </nav>
+  );
+};
+
+export default NavBar;
